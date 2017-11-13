049b7f20569be5155115f8c457a68754320f17c3
IgorMatsepura <Igor.Matsepura@gmail.com>
2017-11-13 10:34:00 +0200

    homeWrk7 v3.0

--- main.cpp
+++ main.cpp
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#define INPUT_NUMBER 1000       // Variable Max input Letters
+#define FIRST_SMALL_LETTER 'a'  // First letter small in alphabet
+#define FIRST_BIG_LETTER 'A'    // First letter big in alphabet
+#define LETTERS_ALPHABET 26     // Number of letters in alphabet
 
-void encryptLetters(char input[], int keyNumber);
-void decryptLetters(char* input, int keyNumber);
+void encryptLetters(char input[], int keyNumber);   //function code letter
+void decryptLetters(char* input, int keyNumber);    // function decode letter
 
 int main()
 {
-char inputWords[1000];
+char inputWords[INPUT_NUMBER];
 int enterNumb = 0;
 
     for (;;){
@@ -23,16 +27,17 @@ int enterNumb = 0;
     }
 }
 
+ //function code letter
 void encryptLetters(char input[], int keyNumber){
     int i = 0;
 
     for (i = 0; i < strlen(input); i++){
         if (islower(input[i])){
-            input[i] = (input[i] - 'a' + keyNumber) % 26;
-            input[i] = input[i] + 'a';
+            input[i] = (input[i] - FIRST_SMALL_LETTER + keyNumber) %LETTERS_ALPHABET;
+            input[i] = input[i] + FIRST_SMALL_LETTER;
         } else {
-            input[i] = (input[i] - 'A' + keyNumber) % 26;
-            input[i] = input[i] + 'A';
+            input[i] = (input[i] - FIRST_BIG_LETTER + keyNumber) %LETTERS_ALPHABET;
+            input[i] = input[i] + FIRST_BIG_LETTER;
         }
     }
     printf("new encrypt words is - ");
@@ -42,20 +47,19 @@ void encryptLetters(char input[], int keyNumber){
     printf("\n");
 }
 
-
-
+// function decode letter
 void decryptLetters(char* input, int keyNumber){
   int i = 0;
   int countWordsAscii;
 
     for (i = 0; i < strlen(input); i++){
         if (islower(input[i])){
-            countWordsAscii = int(input[i])-int('a');
-                input[i] = (input[i] - 'a' - keyNumber + 26 ) %26 ;
-                input[i] = input[i] + 'a';
+            countWordsAscii = int(input[i])-int(FIRST_SMALL_LETTER);
+                input[i] = (input[i] - FIRST_SMALL_LETTER - keyNumber + LETTERS_ALPHABET ) %LETTERS_ALPHABET ;
+                input[i] = input[i] + FIRST_SMALL_LETTER;
         } else {
-            input[i] = (input[i] - 'A' - keyNumber + 26) % 26;
-            input[i] = input[i] + 'A';
+            input[i] = (input[i] - FIRST_BIG_LETTER - keyNumber + LETTERS_ALPHABET) %LETTERS_ALPHABET;
+            input[i] = input[i] + FIRST_BIG_LETTER;
         }
     }
     printf("new decode words is - ");
@@ -64,9 +68,3 @@ void decryptLetters(char* input, int keyNumber){
     }
     printf("\n");
 }
-
-
-
-
-
-
